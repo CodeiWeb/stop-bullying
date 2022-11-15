7265b8b538c38ea7047f2006b0e578830db348de
CodeiWeb <codeiw3b@gmail.com>
2022-11-15 09:06:19 -0300

    mobile - 320

---
+++ script.js
@@ -0,0 +1,9 @@
+const btnmobile = document.getElementById('btn-mobile'); // selecionando o id btn-mobile que esta no html
+
+function clicouNoMenu() {  // função '' clicouNoMenu''
+  const nav = document.getElementById('nav'); // selecionando o id nav
+  nav.classList.toggle('active')  // Adicione a class 'active' caso não tenha esta função, caso tenha remova a class 'active' 
+}
+
+
+btnmobile.addEventListener('click', clicouNoMenu); // evento ao clicar ative a função '' clicouNoMenu''
